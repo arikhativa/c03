ad7da461a65934d05c81d283ccff213c0e0d49fe
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-23 10:18:42 +0200

    ex03

--- ex03/ft_strncat.c
+++ ex03/ft_strncat.c
@@ -1,29 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/22 15:26:19 by yrabby            #+#    #+#             */
-/*   Updated: 2022/05/23 10:10:04 by yrabby           ###   ########.fr       */
+/*   Created: 2022/05/23 10:12:37 by yrabby            #+#    #+#             */
+/*   Updated: 2022/05/23 10:17:57 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcat(char *dest, char *src)
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-	char	*runner;
+	char			*runner;
+	unsigned int	i;
 
+	i = 0;
 	if (!dest || !src)
 		return (dest);
 	runner = dest;
 	while (*runner)
 		++runner;
-	while (*src)
+	while (i < nb && *src)
 	{
 		*runner = *src;
 		++runner;
 		++src;
+		++i;
 	}
 	*runner = 0;
 	return (dest);
