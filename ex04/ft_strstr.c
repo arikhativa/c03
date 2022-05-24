15b68564d16352d104adb5049bfda65545cd027f
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-24 13:54:12 +0200

    V2 is good

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 10:19:12 by yrabby            #+#    #+#             */
-/*   Updated: 2022/05/23 13:57:20 by yrabby           ###   ########.fr       */
+/*   Updated: 2022/05/24 13:54:02 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	}
 	if (i == n)
 		return (0);
-	return ((unsigned char)(*s1) - (unsigned char)(*s2));
+	return (*s1 - *s2);
 }
 
 unsigned int	ft_strlen(char *s)
@@ -44,7 +44,7 @@ char	*ft_strstr(char *str, char *to_find)
 {
 	unsigned int	len;
 
-	if (!to_find)
+	if (!to_find || !(*to_find))
 		return (str);
 	len = ft_strlen(to_find);
 	while (*str)
